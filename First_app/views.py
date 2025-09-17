8ac4900ede517587ece25a4997694820c48b263e
Keyur <keyurkhant0@gmail.com>
2025-09-17 21:52:58 +0530

    My first Django app

---
+++ First_app/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello,World!")
+
+def Keyur(request):
+    return HttpResponse("Hello,Keyur!")
+def Greet(request,name):
+    return HttpResponse(f"Hello,{name.capitalize()}")
